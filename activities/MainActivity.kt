1edf7a2f948cb17c8024292b2ec8377f32561546
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2021-06-13 01:01:32 +0600

    main screen activity (onClickListener doesnt work)

--- activities/MainActivity.kt
+++ activities/MainActivity.kt
@@ -1,26 +1,54 @@
 package com.example.myapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toolbar
+import android.widget.*
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapp.adapters.MyAdapter
+import com.example.myapp.models.CardModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var card : CardView
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        card = findViewById(R.id.card_view)
-        card.setOnClickListener {
-            Log.d("card", "nunu tipse")
+        val list = ArrayList <CardModel> (100)
+
+        for(i in 1..10) {
+//            val card = CardModel (i.toString(), (i*i).toString(), (i*i*i).toString())
+            val card = CardModel ("Article Name", "Website Name", "Date Added")
+            list.add(card)
+        }
+
+        Log.d("size", list.size.toString())
+
+        recyclerView = findViewById(R.id.recycler_view)
+        val adapter = MyAdapter(list, this)
+        recyclerView.adapter = adapter
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+
+
+//        recyclerView.setOnClickListener { parent:AdapterView
+//
+//        }
+    }
+
+    companion object {
+        public fun onClickListener_Cards(articleName : String) {
+//            Toast.makeText(this, articleName, Toast.LENGTH_SHORT).show()
+            Log.d("dhur", articleName)
         }
     }
 }
