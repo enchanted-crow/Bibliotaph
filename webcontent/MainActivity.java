ea353515d0453fbda20e34d84589d15c6ae08ea5
Tahmid <47899219+TahmidMahin@users.noreply.github.com>
2021-06-14 02:07:46 +0600

    Update MainActivity.java

--- webcontent/MainActivity.java
+++ webcontent/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.editText);
         FloatingActionButton addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, WebActivity.class);
@@ -24,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
-        String articleName = intent.getStringExtra(WebActivity.MSG);
-        textView.setText(articleName);
+        String articleBody = intent.getStringExtra(WebActivity.MSG);
+        editText.setText(articleBody);
 
     }
 
