11bbf6613af8dfc3d0a2520574051a016053e9d2
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2021-06-15 02:29:43 +0600

    Add files via upload

---
+++ myapp/WebActivity.java
@@ -0,0 +1,67 @@
+package com.example.myapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class WebActivity extends AppCompatActivity {
+
+    public static final String TITLE = "com.example.pdfFinder.TITLE";
+    public static final String BODY = "com.example.pdfFinder.BODY";
+    private WebView webView;
+
+    private void extractTextArticle(String url) {
+        new Thread(() -> {
+            try {
+                StringBuilder articleBody = new StringBuilder();
+                Document document = Jsoup.connect(url).get();
+                String title = document.title();
+                Element body = document.getElementById("mw-content-text");
+                Elements paragraphs = body.getElementsByTag("p");
+                for(Element paragraph: paragraphs) {
+                    articleBody.append(paragraph.text()).append("\n");
+                }
+
+                Intent intent = new Intent(WebActivity.this, MainActivity.class);
+                intent.putExtra(TITLE, title);
+                intent.putExtra(BODY, articleBody.toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            String url = webView.getUrl();
+            Log.i("url", url);
+            extractTextArticle(url);
+        });
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().getJavaScriptEnabled();
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://en.wikipedia.org/");
+    }
+}
---
+++ myapp/models/CardModel.kt
@@ -0,0 +1,3 @@
+package com.example.myapp.models
+
+data class CardModel (var fileName : String, val dateAdded : String?) {}
