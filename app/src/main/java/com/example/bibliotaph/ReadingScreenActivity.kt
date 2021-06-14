1e5eb6e7f5f261c68a2b151302c16b5b7b76f155
Jawad <jrmrmh@gmail.com>
2021-06-15 03:37:04 +0600

    tryna

---
+++ app/src/main/java/com/example/bibliotaph/ReadingScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bibliotaph
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReadingScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reading_screen)
+    }
+}
