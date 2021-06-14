d71800e7dab978dddd4bf4173cb04bc49a4715d5
Tahmid <47899219+TahmidMahin@users.noreply.github.com>
2021-06-14 22:24:30 +0600

    Update Article.java

--- pdffinder/model/Article.java
+++ pdffinder/model/Article.java
@@ -3,12 +3,14 @@ package com.example.pdffinder.model;
 public class Article {
     private String fileName;
     private String textBody;
-
+    private String dateAdded;
 
     public Article() {}
-    public Article(String fileName, String textBody) {
+
+    public Article(String fileName, String textBody, String dateAdded) {
         this.fileName = fileName;
         this.textBody = textBody;
+        this.dateAdded = dateAdded;
     }
 
     public String getFileName() {
@@ -25,5 +27,13 @@ public class Article {
 
     public void setTextBody(String textBody) {
         this.textBody = textBody;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
     }
 }
