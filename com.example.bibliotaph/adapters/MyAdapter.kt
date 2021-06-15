94277a8722d5feda36f33f5dd1fd8a827cc0aeeb
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2021-06-15 11:20:03 +0600

    Update MyAdapter.kt

--- com.example.bibliotaph/adapters/MyAdapter.kt
+++ com.example.bibliotaph/adapters/MyAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.bibliotaph.adapters
 
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,31 +10,46 @@ import com.example.bibliotaph.R
 import com.example.bibliotaph.models.CardModel
 
 data class MyAdapter(
-	private var list: ArrayList <CardModel>?,
-	private var context: Context?
+		private var cardList: ArrayList <CardModel>,
+		private val onCardListener : OnCardListener
 		) : RecyclerView.Adapter <MyAdapter.MyViewHolder> () {
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-		val view : View = LayoutInflater.from(context).inflate(R.layout.recyclerview, parent, false)
+		val view : View = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
 		return MyViewHolder(view)
 	}
 
 	override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-		val cardModel : CardModel = list!![position]
+		val cardModel = cardList[position]
 		holder.articleName.text = cardModel.fileName
 		holder.dateAdded.text = cardModel.dateAdded
+		Log.d("recyclerview", ": binded $position")
 	}
 
 	override fun getItemCount(): Int {
-		return list!!.size
+		return cardList.size
 	}
 
-	class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+	inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 		var articleName : TextView = itemView.findViewById(R.id.article_name)
 		var dateAdded : TextView = itemView.findViewById(R.id.date_added)
+
+		init {
+			itemView.setOnClickListener(this)
+
+			Log.d("recyclerview", ": holder init ")
+		}
+
+		override fun onClick(v: View) {
+			if(adapterPosition != RecyclerView.NO_POSITION) {
+				onCardListener.onCardClick(adapterPosition)
+
+				Log.d("recyclerview", ": onclick  ")
+			}
+		}
 	}
 
-//	interface OnCardListener {
-//		fun onCardListener (position: Int)
-//	}
+	interface OnCardListener {
+		fun onCardClick (position: Int)
+	}
 }
