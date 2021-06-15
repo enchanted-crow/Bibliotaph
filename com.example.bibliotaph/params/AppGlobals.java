e10c48342f1d6f69864323d1f25369051802ce0a
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2021-06-15 11:20:57 +0600

    Update AppGlobals.java

--- com.example.bibliotaph/params/AppGlobals.java
+++ com.example.bibliotaph/params/AppGlobals.java
@@ -1,19 +1,81 @@
-package com.example.bibliotaph.params;
+package com.example.bibliotaph;
 
-import com.example.bibliotaph.DbHandler;
-import com.example.bibliotaph.models.CardModel;
-
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import com.example.bibliotaph.models.Article;
+import com.example.bibliotaph.params.AppGlobals;
 import java.util.ArrayList;
 
-public class AppGlobals {
-    public static final int DB_VERSION = 1;
-    public static final String DB_NAME = "articleinfo";
-    public static final String TABLE_NAME = "articleinfo";
+public class DbHandler extends SQLiteOpenHelper {
 
+    public DbHandler(Context context) {
+        super(context, AppGlobals.DB_NAME, null, AppGlobals.DB_VERSION);
+    }
 
-    public static final String KEY_NAME = "filename";
-    public static final String KEY_BODY = "textbody";
-    public static final String KEY_DATE = "added";
-    public static DbHandler myDB = null;
-    public static ArrayList <CardModel> cardList = new ArrayList <CardModel> (1000);
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "Create table if not exists " + AppGlobals.TABLE_NAME + "(" +
+                AppGlobals.KEY_NAME + " Varchar Primary key, " + AppGlobals.KEY_BODY +
+                " Text, " + AppGlobals.KEY_DATE + " Datetime)";
+        db.execSQL(create);
+        Log.i("database", "Database created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addArticle(Article article) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(AppGlobals.KEY_NAME, article.getFileName());
+        values.put(AppGlobals.KEY_BODY, article.getTextBody());
+        values.put(AppGlobals.KEY_DATE, article.getDateAdded());
+        db.insert(AppGlobals.TABLE_NAME, null, values);
+        Log.i("database", article.getTextBody());
+    }
+
+    public ArrayList<Article> getAllArticles() {
+        ArrayList<Article> articleList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String select = "Select * from " + AppGlobals.TABLE_NAME;
+        Cursor cursor = db.rawQuery(select, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                Article article = new Article();
+                article.setFileName(cursor.getString(0));
+                article.setTextBody(cursor.getString(1));
+                article.setDateAdded(cursor.getString(2));
+                articleList.add(article);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return articleList;
+    }
+
+    public String getTextBodyFromFilename(String fileName) {
+        String query = "Select " + AppGlobals.KEY_BODY + " from " + AppGlobals.TABLE_NAME + " WHERE " + AppGlobals.KEY_NAME + " = \"" + fileName + "\"";
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            try {
+                String ret = cursor.getString(0);
+                cursor.close();
+                return ret;
+            } catch (Exception e) {
+                e.printStackTrace();
+                cursor.close();
+            }
+        }
+        return "ERROR!";
+    }
 }
