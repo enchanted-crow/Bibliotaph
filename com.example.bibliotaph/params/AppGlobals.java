c541ac9c75e1be24227ea194d246adc93ab8a900
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2021-06-15 11:20:39 +0600

    Update and rename Params.java to AppGlobals.java

--- com.example.bibliotaph/params/AppGlobals.java
+++ com.example.bibliotaph/params/AppGlobals.java
@@ -1,6 +1,11 @@
 package com.example.bibliotaph.params;
 
-public class Params {
+import com.example.bibliotaph.DbHandler;
+import com.example.bibliotaph.models.CardModel;
+
+import java.util.ArrayList;
+
+public class AppGlobals {
     public static final int DB_VERSION = 1;
     public static final String DB_NAME = "articleinfo";
     public static final String TABLE_NAME = "articleinfo";
@@ -9,5 +14,6 @@ public class Params {
     public static final String KEY_NAME = "filename";
     public static final String KEY_BODY = "textbody";
     public static final String KEY_DATE = "added";
-
+    public static DbHandler myDB = null;
+    public static ArrayList <CardModel> cardList = new ArrayList <CardModel> (1000);
 }
